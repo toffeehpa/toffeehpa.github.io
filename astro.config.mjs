ff314a32e844ab616c8d36c57af684fa6a6e753e
Toffincom <128030222+toffeehpa@users.noreply.github.com>
2025-12-26 14:53:07 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://toffeehpa.github.io',
-  // base: '/'  // Не обязательно для репозитория username.github.io, но можно оставить
+  build: {
+    assets: 'assets' // Я надеюсь ты заработаешь теперь сука
+  }
 });
